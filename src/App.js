9b9dd3877f4e356be265a8ec31f28db5fe2f7eab
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-04-01 17:54:23 +0300

    add hanle Amount change functions

--- src/App.js
+++ src/App.js
@@ -9,7 +9,19 @@ function App() {
   const [currencyOptions, SetCurrencyOptions] = useState([])
   const[fromCurrency, setFromCurrency] = useState();
   const[toCurrency, setToCurrency] = useState();
+  const[exchangeRate,setExchangeRate] = useState();
   const[amount,setAmount] = useState(1);
+  const[amountInFromCurrency,setAmountInFromCurrency] = useState(true)
+  
+  let toAmount, fromAmount
+  if(amountInFromCurrency){
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  }else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
 
   useEffect(()=>{
     fetch(BASE_URL)
@@ -19,8 +31,19 @@ function App() {
         SetCurrencyOptions([data.base, ...Object.keys(data.rates)])
         setFromCurrency(data.base)
         setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
       })
   }, [])
+
+  function handleFromAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <>
@@ -29,12 +52,16 @@ function App() {
       currencyOptions = {currencyOptions}
       selectedCurrency={fromCurrency}
       onChangeCurrency={e =>setFromCurrency(e.target.value)}
+      onChangeAmount={handleFromAmountChange}
+      amount = {fromAmount}
     />
     <div className='equals'>=</div>
     <CurrencyRow
       currencyOptions = {currencyOptions}
       selectedCurrency={toCurrency}
       onChangeCurrency={e =>setToCurrency(e.target.value)}
+      onChangeAmount={handleToAmountChange}
+      amount = {toAmount}
     />
     </>
     
